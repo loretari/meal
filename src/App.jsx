9e5e8a88d713110f2ce905df0c2d81dc918ac3cd
loretari <loretari@gmail.com>
2022-10-17 12:24:19 +0300

    init Meal

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import Favorites from './components/Favorites'
+import Meals from './components/Meals'
+import Modal from './components/Modal'
+import Search from './components/Search'
+import './App.css'
+
+export default function App() {
+    return (
+        <main>
+            {/*Meals App*/}
+            {/*/!*  <Search/> *!/*/}
+            {/*/!* <Favorites /> *!/*/}
+
+            <Meals />
+            {/*/!* <Modal/> *!/*/}
+        </main>
+    )
+}
---
+++ src/components/Modal.jsx
@@ -0,0 +1,6 @@
+const Modal = () => {
+    return
+    <h1>Modal Component</h1>
+}
+
+export default Modal
