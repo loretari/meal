2bf022a46b2072ac20d1959dc81a6feae8611ce8
loretari <loretari@gmail.com>
2022-10-18 10:38:31 +0300

    init: add search jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ export default function App() {
     return (
         <main>
             {/*Meals App*/}
-            {/*/!*  <Search/> *!/*/}
+              <Search/>
             {/*/!* <Favorites /> *!/*/}
 
             <Meals />
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,29 @@
 
+import { useState } from 'react';
+import { useGlobalContext } from "../context";
+
 const Search = () => {
-    return
-    <h1>Search Component</h1>
+
+    const [text, setText] = useState('')
+
+    const handleChange = (e) => {
+        setText(e.target.value)
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
+
+
+
+
+    return <header className = "search-container">
+        <form onSubmit= {handleSubmit}>
+            <input type = "text" onChange={handleChange} value= {text} placeholder = 'type favorite meal' claaName = 'form-input'/>
+            <button type = "submit" claaName = 'btn'>Search</button>
+            <button type = "button" claaName = 'btn btn-hipster'>Surprise me!</button>
+        </form>
+    </header>
 }
 
 export default Search
